20aa58b46c80b04e8cd62c3671dc625bcc7612b3
BIK333 <brandk0313@gmail.com>
2025-10-23 09:05:56 -0400

    Update Program.cs

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        double sum = 0;
+        List<double> numbers = new List<double>();
+
+        Console.Write("n = ");
+        int.TryParse(Console.ReadLine(), out int n);
+
+        if (n == 0)
+        {
+            Console.WriteLine("No result.");
+            return;
+        }
+
+        int i = 0;
+        do
+        {
+            Console.Write("a = ");
+            double.TryParse(Console.ReadLine(), out double a);
+            sum += a;
+            numbers.Add(a);
+            i++;
+        }
+        while (i != n);
+
+        double mean = sum / n;
+
+        numbers.Sort();
+
+        
+        double median;
+        if (n % 2 == 1)
+        {
+            median = numbers[n / 2];
+        }
+        else
+        {
+            median = (numbers[(n / 2) - 1] + numbers[n / 2]) / 2.0;
+        }
+
+        double min = numbers[0];
+        double max = numbers[n - 1];
+
+        Console.WriteLine($"Mean:   {mean:F2}");
+        Console.WriteLine($"Median: {median:F2}");
+        Console.WriteLine($"Min:    {min:F2}");
+        Console.WriteLine($"Max:    {max:F2}");
+    }
+}
